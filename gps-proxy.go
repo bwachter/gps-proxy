54abe5ebd6f570aa7bee2b639f61d892644d5933
Bernd Wachter <bernd-github@wachter.fi>
2021-07-12 14:47:59 +0300

    Add support for calculating checksums

--- gps-proxy.go
+++ gps-proxy.go
@@ -32,6 +32,24 @@ type ConfigData struct {
 var configData ConfigData
 var mqttClient mqtt.Client
 
+func NMEAchecksum(in string) string {
+	// this assumes we get a complete string, with checksum at the end
+	// it'd probably be sensible to eventually add checks for what is
+	// actually received.
+
+	// chop off checksum
+	in = in[:len(in)-3]
+	// chop off $ at the beginning
+	in = in[1:]
+
+	var checksum byte = 0
+	for i := 0; i < len(in); i++ {
+		checksum ^= in[i]
+	}
+
+	return fmt.Sprintf("%X", checksum)
+}
+
 func gpsTimeToUTC(in string) string {
 	if len(in) != 9 {
 		return ""
@@ -139,74 +157,83 @@ func handleData(c net.Conn) {
 
 		line := strings.TrimSpace(string(data))
 
-		// this will be needed
-		//checksum := line[len(line)-2:]
+		// minimum expected line would be something like $GPGSA,*CC -
+		// which is 10 characters. Though even that short one probably
+		// should never happen.
+		if len(line) < 10 {
+			fmt.Printf("[%s] Unexpected short line: %s\n", client_name, line)
+			continue
+		}
+
+		calculated_checksum := NMEAchecksum(line)
+		received_checksum := line[len(line)-2:]
+
+		if calculated_checksum != received_checksum {
+			fmt.Printf("Checksum mismatch, calculated %s but got %s\n",
+				calculated_checksum, received_checksum)
+			continue
+		}
 
 		// chop off checksum, assuming there's always a checksum
 		line = line[:len(line)-3]
+		values := strings.Split(line, ",")
+		len := len(values)
 
-		if len(line) < 6 {
-			fmt.Printf("[%s] Unexpected short line: %s\n", client_name, line)
-		} else {
-			values := strings.Split(line, ",")
-			len := len(values)
-
-			// TODO: cut off checksum and calculate
-			switch values[0] {
-			case "$GPGGA":
-				if gpsDDMTo("DMS", values[2], values[3]) != "EDIR" &&
-					gpsDDMTo("DMS", values[4], values[5]) != "EDIR" {
-					publish(base_topic, "GPGGA/utc", gpsTimeToUTC(values[1]))
-					publish(base_topic, "GPGGA/lat_dms", gpsDDMTo("DMS", values[2], values[3]))
-					publish(base_topic, "GPGGA/lon_dms", gpsDDMTo("DMS", values[4], values[5]))
-					publish(base_topic, "GPGGA/lat_dd", gpsDDMTo("DD", values[2], values[3]))
-					publish(base_topic, "GPGGA/lon_dd", gpsDDMTo("DD", values[4], values[5]))
-					publish(base_topic, "GPGGA/quality", values[6])
-					publish(base_topic, "GPGGA/sats", values[7])
-					publish(base_topic, "GPGGA/hdop", values[8])
-					publish(base_topic, "GPGGA/alt", values[9])
-					publish(base_topic, "GPGGA/alt_unit", values[10])
-					// undulation and differential data is ignored for now
-				}
-			case "$GPRMC":
-				// this should only be sent with a valid fix, so skip accuracy information
-				// check status header
-				// add track true, mag var, var dir, mode ind fields
-				fmt.Printf("[%s] GPRMC: %s, %s %s speed %s\n", client_name,
-					gpsTimeToUTC(values[1]),
-					gpsDDMTo("DMS", values[3], values[4]),
-					gpsDDMTo("DMS", values[5], values[6]),
-					gpsSpeedToKmh(values[7]),
-				)
-				gps_time := gpsDateTime(values[9], values[1])
-				publish(base_topic, "GPRMC/time", gpsTimeToUTC(values[1]))
-				publish(base_topic, "GPRMC/lat_dms", gpsDDMTo("DMS", values[3], values[4]))
-				publish(base_topic, "GPRMC/lon_dms", gpsDDMTo("DMS", values[5], values[6]))
-				publish(base_topic, "GPRMC/lat_dd", gpsDDMTo("DD", values[3], values[4]))
-				publish(base_topic, "GPRMC/lon_dd", gpsDDMTo("DD", values[5], values[6]))
-				publish(base_topic, "GPRMC/speed_kmh", gpsSpeedToKmh(values[7]))
-				publish(base_topic, "GPRMC/date_time", gps_time.Format("2006-01-02 15:04:05"))
-				publish(base_topic, "GPRMC/date_time_local", gps_time.Local().Format("2006-01-02 15:04:05"))
-			case "$GPGSA":
-				publish(base_topic, "GPGSA/field_length", strconv.Itoa(len))
-				publish(base_topic, "GPGSA/mode_ma", values[1])
-				publish(base_topic, "GPGSA/mode_123", values[2])
-				// TODO: print satellite info in fields 3-14
-				publish(base_topic, "GPGSA/pdop", values[15])
-				publish(base_topic, "GPGSA/hdop", values[16])
-				publish(base_topic, "GPGSA/vdop", values[17])
-			case "$GPGSV":
-				publish(base_topic, "GPGSV/field_length", strconv.Itoa(len))
-			case "$GPVTG":
-				publish(base_topic, "GPVTG/track_true", values[1])
-				publish(base_topic, "GPVTG/track_mag", values[3])
-				publish(base_topic, "GPVTG/speed_kn", values[5])
-				publish(base_topic, "GPVTG/speed_kmh", values[7])
-				publish(base_topic, "GPVTG/mode_indicator", values[9])
-			default:
-				fmt.Printf("[%s] Unexpected line: %s\n", client_name, line)
+		switch values[0] {
+		case "$GPGGA":
+			if gpsDDMTo("DMS", values[2], values[3]) != "EDIR" &&
+				gpsDDMTo("DMS", values[4], values[5]) != "EDIR" {
+				publish(base_topic, "GPGGA/utc", gpsTimeToUTC(values[1]))
+				publish(base_topic, "GPGGA/lat_dms", gpsDDMTo("DMS", values[2], values[3]))
+				publish(base_topic, "GPGGA/lon_dms", gpsDDMTo("DMS", values[4], values[5]))
+				publish(base_topic, "GPGGA/lat_dd", gpsDDMTo("DD", values[2], values[3]))
+				publish(base_topic, "GPGGA/lon_dd", gpsDDMTo("DD", values[4], values[5]))
+				publish(base_topic, "GPGGA/quality", values[6])
+				publish(base_topic, "GPGGA/sats", values[7])
+				publish(base_topic, "GPGGA/hdop", values[8])
+				publish(base_topic, "GPGGA/alt", values[9])
+				publish(base_topic, "GPGGA/alt_unit", values[10])
+				// undulation and differential data is ignored for now
 			}
+		case "$GPRMC":
+			// this should only be sent with a valid fix, so skip accuracy information
+			// check status header
+			// add track true, mag var, var dir, mode ind fields
+			fmt.Printf("[%s] GPRMC: %s, %s %s speed %s\n", client_name,
+				gpsTimeToUTC(values[1]),
+				gpsDDMTo("DMS", values[3], values[4]),
+				gpsDDMTo("DMS", values[5], values[6]),
+				gpsSpeedToKmh(values[7]),
+			)
+			gps_time := gpsDateTime(values[9], values[1])
+			publish(base_topic, "GPRMC/time", gpsTimeToUTC(values[1]))
+			publish(base_topic, "GPRMC/lat_dms", gpsDDMTo("DMS", values[3], values[4]))
+			publish(base_topic, "GPRMC/lon_dms", gpsDDMTo("DMS", values[5], values[6]))
+			publish(base_topic, "GPRMC/lat_dd", gpsDDMTo("DD", values[3], values[4]))
+			publish(base_topic, "GPRMC/lon_dd", gpsDDMTo("DD", values[5], values[6]))
+			publish(base_topic, "GPRMC/speed_kmh", gpsSpeedToKmh(values[7]))
+			publish(base_topic, "GPRMC/date_time", gps_time.Format("2006-01-02 15:04:05"))
+			publish(base_topic, "GPRMC/date_time_local", gps_time.Local().Format("2006-01-02 15:04:05"))
+		case "$GPGSA":
+			publish(base_topic, "GPGSA/field_length", strconv.Itoa(len))
+			publish(base_topic, "GPGSA/mode_ma", values[1])
+			publish(base_topic, "GPGSA/mode_123", values[2])
+			// TODO: print satellite info in fields 3-14
+			publish(base_topic, "GPGSA/pdop", values[15])
+			publish(base_topic, "GPGSA/hdop", values[16])
+			publish(base_topic, "GPGSA/vdop", values[17])
+		case "$GPGSV":
+			publish(base_topic, "GPGSV/field_length", strconv.Itoa(len))
+		case "$GPVTG":
+			publish(base_topic, "GPVTG/track_true", values[1])
+			publish(base_topic, "GPVTG/track_mag", values[3])
+			publish(base_topic, "GPVTG/speed_kn", values[5])
+			publish(base_topic, "GPVTG/speed_kmh", values[7])
+			publish(base_topic, "GPVTG/mode_indicator", values[9])
+		default:
+			fmt.Printf("[%s] Unexpected line: %s\n", client_name, line)
 		}
+
 	}
 	c.Close()
 }
